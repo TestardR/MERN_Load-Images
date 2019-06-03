769abae0cf2314406ef82ab3d8fa9ed2660e7142
unknown <rmtestard@gmail.com>
2019-06-03 19:17:10 +0200

    back end

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
+const productRoutes = require('./api/routes/products');
+
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// CORS
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use('/products', productRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
